9345804d43d18f25651d0a04356cebee62046d2e
arpit <arpit.biswas@houseclay.com>
2023-05-05 19:34:22 +0530

    Initial commit

---
+++ src/main/java/com/houseclay/receiptbuilder/ReceiptbuilderApplication.java
@@ -0,0 +1,13 @@
+package com.houseclay.receiptbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptbuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptbuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/houseclay/receiptbuilder/ReceiptbuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.houseclay.receiptbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptbuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
